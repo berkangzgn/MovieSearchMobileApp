fde68986d062ff869ae80e4c5d6a87794c45cb24
berkangzgn <bgezgin@protonmail.com>
2022-01-22 15:17:51 +0300

    Initial Commit

---
+++ MovieSearchMobileApp/MovieSearchMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchMobileAppApp.swift
+//  MovieSearchMobileApp
+//
+//  Created by Berkan Gezgin on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
